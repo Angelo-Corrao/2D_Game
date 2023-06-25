b082739d72e94cb2db370b5fceecc591e33fc9dc
Angelo Corrao <precipitevoliss1@gmail.com>
2023-06-25 11:04:05 +0200

    Added movement animation
    When the player press a key to move in no more teleporting to the destination but it moves by little steps

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -7,11 +8,17 @@ public class CarMovement : MonoBehaviour
     public Tilemap fogOfWar;
 
     private PlayerInput playerInput;
+	private Vector2 direction; // rompe tutto va passato come parametro
 	private string previousDirection = "";
+	private bool isMoving = false;
+	private bool checkCurve = false;
 
 	private void Awake() {
 		playerInput = new PlayerInput();
-		playerInput.Car.Movement.performed += ctx => CanMove(ctx.ReadValue<Vector2>());
+		playerInput.Car.Movement.performed += ctx => {
+			direction = ctx.ReadValue<Vector2>();
+			CanMove();
+		};
 	}
 
 	private void OnEnable() {
@@ -22,9 +29,98 @@ public class CarMovement : MonoBehaviour
 		playerInput.Disable();
 	}
 
-	private void Move(Vector2 direction, Vector3Int nextGridPosition) {
-		transform.position += (Vector3)direction;
+	private void Update() {
+		/*
+		 * Mangage the player position if he is in a curve (tunnel) after he moved
+		 * The player is moved in another cell based on the direction from which it comes
+		 * When it will no longer be in a curve the loop will end
+		 */
+		if (checkCurve) {
+			if (!isMoving) {
+				Vector3Int actualGridPosition;
+				Vector3Int nextGridPosition;
+				actualGridPosition = ground.WorldToCell(transform.position);
+				switch (ground.GetTile(actualGridPosition).name) {
+					case "roadNE":
+						if (previousDirection == "down") {
+							direction = new Vector2(1, 0);
+							nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
+							Move(nextGridPosition);
+							previousDirection = "right";
+						}
+						else {
+							direction = new Vector2(0, 1);
+							nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
+							Move(nextGridPosition);
+							previousDirection = "up";
+						}
+						break;
+
+					case "roadNW":
+						if (previousDirection == "down") {
+							direction = new Vector2(-1, 0);
+							nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
+							Move(nextGridPosition);
+							previousDirection = "left";
+						}
+						else {
+							direction = new Vector2(0, 1);
+							nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
+							Move(nextGridPosition);
+							previousDirection = "up";
+						}
+						break;
+
+					case "roadSE":
+						if (previousDirection == "up") {
+							direction = new Vector2(1, 0);
+							nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
+							Move(nextGridPosition);
+							previousDirection = "right";
+						}
+						else {
+							direction = new Vector2(0, -1);
+							nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
+							Move(nextGridPosition);
+							previousDirection = "down";
+						}
+						break;
+
+					case "roadSW":
+						if (previousDirection == "up") {
+							direction = new Vector2(-1, 0);
+							nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
+							Move(nextGridPosition);
+							previousDirection = "left";
+						}
+						else {
+							direction = new Vector2(0, -1);
+							nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
+							Move(nextGridPosition);
+							previousDirection = "down";
+						}
+						break;
+				}
+
+				if (ground.GetTile(actualGridPosition).name != "roadNE" &&
+					ground.GetTile(actualGridPosition).name != "roadNW" &&
+					ground.GetTile(actualGridPosition).name != "roadSE" &&
+					ground.GetTile(actualGridPosition).name != "roadSW")
+					checkCurve = false;
+			}
+		}
+	}
+
+	private void Move(Vector3Int nextGridPosition) {
+		isMoving = true;
 		fogOfWar.SetTile(nextGridPosition, null);
+
+		int stepCounter = 0;
+		float totSteps = 10;
+		float animationTime = 0.5f;
+		float oneStepDistance = 0.1f;
+		float timeBetweenSteps = animationTime / totSteps;
+		StartCoroutine(Animation(direction, stepCounter, oneStepDistance, timeBetweenSteps));
 
 		if (direction.x == 0) {
 			if (direction.y == 1)
@@ -38,7 +134,20 @@ public class CarMovement : MonoBehaviour
 			previousDirection = "left";
 	}
 
-	private void CanMove(Vector2 direction) {
+	private IEnumerator Animation(Vector2 direction, float stepCounter, float oneStepDistance, float timeBetweenSteps) {
+		transform.position += (Vector3)direction * oneStepDistance;
+		stepCounter++;
+		yield return new WaitForSeconds(timeBetweenSteps);
+		if (stepCounter < 10)
+			StartCoroutine(Animation(direction, stepCounter, oneStepDistance, timeBetweenSteps));
+		else
+			isMoving = false;
+	}
+
+	private void CanMove() {
+		if (isMoving)
+			return;
+
 		Vector3Int actualGridPosition = ground.WorldToCell(transform.position);
 		Vector3Int nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
 
@@ -46,104 +155,32 @@ public class CarMovement : MonoBehaviour
 		switch (ground.GetTile(actualGridPosition).name) {
 			case "roadNEWS":
 				// Here the player can move in any direction
-				if (isCellNotEmpty(nextGridPosition, direction))
-					Move(direction, nextGridPosition);
+				if (isCellNotEmpty(nextGridPosition))
+					Move(nextGridPosition);
 				break;
 
 			case "roadEW":
 				// Check if the player moved horizontally
 				if (direction.y == 0) {
-					if (isCellNotEmpty(nextGridPosition, direction))
-						Move(direction, nextGridPosition);
+					if (isCellNotEmpty(nextGridPosition))
+						Move(nextGridPosition);
 				}
 				break;
 
 			case "roadNS":
 				// Check if the player moved vertically
 				if (direction.x == 0) {
-					if (isCellNotEmpty(nextGridPosition, direction))
-						Move(direction, nextGridPosition);
+					if (isCellNotEmpty(nextGridPosition))
+						Move(nextGridPosition);
 				}
 				break;
 		}
 
-		/*
-		 * Mangage the player position if he is in a curve (tunnel) after he moved
-		 * The player is moved in another cell based on the direction from which it comes
-		 * When it will no longer be in a curve the loop will end
-		 */
-		do {
-			actualGridPosition = ground.WorldToCell(transform.position);
-			switch (ground.GetTile(actualGridPosition).name) {
-				case "roadNE":
-					if (previousDirection == "down") {
-						direction = new Vector2(1, 0);
-						nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
-						Move(direction, nextGridPosition);
-						previousDirection = "right";
-					}
-					else {
-						direction = new Vector2(0, 1);
-						nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
-						Move(direction, nextGridPosition);
-						previousDirection = "up";
-					}
-					break;
-
-				case "roadNW":
-					if (previousDirection == "down") {
-						direction = new Vector2(-1, 0);
-						nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
-						Move(direction, nextGridPosition);
-						previousDirection = "left";
-					}
-					else {
-						direction = new Vector2(0, 1);
-						nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
-						Move(direction, nextGridPosition);
-						previousDirection = "up";
-					}
-					break;
-
-				case "roadSE":
-					if (previousDirection == "up") {
-						direction = new Vector2(1, 0);
-						nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
-						Move(direction, nextGridPosition);
-						previousDirection = "right";
-					}
-					else {
-						direction = new Vector2(0, -1);
-						nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
-						Move(direction, nextGridPosition);
-						previousDirection = "down";
-					}
-					break;
-
-				case "roadSW":
-					if (previousDirection == "up") {
-						direction = new Vector2(-1, 0);
-						nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
-						Move(direction, nextGridPosition);
-						previousDirection = "left";
-					}
-					else {
-						direction = new Vector2(0, -1);
-						nextGridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
-						Move(direction, nextGridPosition);
-						previousDirection = "down";
-					}
-					break;
-			}
-
-			actualGridPosition = ground.WorldToCell(transform.position);
-		} while (ground.GetTile(actualGridPosition).name == "roadNE" ||
-				 ground.GetTile(actualGridPosition).name == "roadNW" ||
-				 ground.GetTile(actualGridPosition).name == "roadSE" ||
-				 ground.GetTile(actualGridPosition).name == "roadSW");
+		// This is nedeed so we can check in the Update if the player has landed in a curve
+		checkCurve = true;
 	}
 
-	private bool isCellNotEmpty(Vector3Int nextGridPosition, Vector2 direction) {
+	private bool isCellNotEmpty(Vector3Int nextGridPosition) {
 		// Check if exist a tile in the ground tilemap in the direction the player want to move
 		if (ground.HasTile(nextGridPosition) && direction.magnitude <= 1) // The second check is nedeed so the player can't move diagonally
 			return true;
